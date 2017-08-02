bc919d928a49006009ea10850fdfff649816b58c
mambax7 <mambax7@gmail.com>
2017-08-01 22:04:34 -0400

    fixed upload, in case folder already exists

--- include/install.php
+++ include/install.php
@@ -21,7 +21,18 @@ function xoops_module_install_fbcomment(XoopsModule $module)
     // global $xoopsDB,$xoopsConfig;
 
     // make upload directory
-    mkdir(XOOPS_ROOT_PATH . '/uploads/fbcomment');
+    $folder = XOOPS_ROOT_PATH . '/uploads/fbcomment';
+    try {
+        if (!file_exists($folder)) {
+            if (!mkdir($folder) && !is_dir($folder)) {
+                throw new \RuntimeException(sprintf('Unable to create the %s directory', $folder));
+            } else {
+                file_put_contents($folder . '/index.html', '<script>history.go(-1);</script>');
+            }
+        }
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n", '<br/>';
+    }
 
     return true;
 }
